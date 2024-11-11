0a9be2384ac82d00b908263ff98fb6c842dce00b
abraham <wole1702@gmail.com>
2024-11-11 23:04:46 +0100

    fixes_added_route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """GET /api/v1/unathorized
+    """
+    abort(401)
