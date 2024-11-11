ff866896636e0a55b206d7fa0ba1ed2ee4f34a92
abraham <wole1702@gmail.com>
2024-11-11 23:39:51 +0100

    added

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+
+    def __init__(self):
+        pass
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ returns False """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return False """
+        return None
