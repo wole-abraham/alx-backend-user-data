44b62d85b9699de6e072b50a66058a744f142ece
abraham <wole1702@gmail.com>
2024-11-15 00:31:43 +0100

    renamed

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Authentication class """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False"""
+        if path is None or len(path) == 0:
+            return True
+        elif excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        elif path:
+            if path[-1] != '/':   
+                path = path + '/'
+            if path in excluded_paths:
+                return False
+            else:
+                return True
+
+    def authorization_header(self, request=None) -> str:
+        """ returns False """
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return False """
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+""" basic_authentication module"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+import base64
+import binascii
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ base64 authorization header """
+        if authorization_header is None:
+            return None
+        if not (isinstance(authorization_header, str)):
+            return None
+        if authorization_header.split()[0] != 'Basic':
+            return None
+        else:
+            return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode base64 authorizaton header  """
+        if base64_authorization_header is None:
+            return None
+        if not (isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        else:
+            return decoded.decode()
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> str:
+        """ Extract user credentials """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not (isinstance(decoded_base64_authorization_header, str)):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        a, b = (x for x in decoded_base64_authorization_header.split(':', 1))
+        return (a, b)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ user object from credential """
+        if user_email is None or user_pwd is None:
+            return None
+        if not (isinstance(user_email, str)):
+            return None
+        if not (isinstance(user_pwd, str)):
+            return None
+        try:
+            user = User.search({'email': user_email})
+        except KeyError:
+            return None
+        if len(user) == 0:
+            return None
+        if user[0].is_valid_password(user_pwd):
+            return user[0]
+        else:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Current users """
+        authorization = self.authorization_header(request)
+        extracted = self.extract_base64_authorization_header(authorization)
+        decode = self.decode_base64_authorization_header(extracted)
+        usr, pwd = self.extract_user_credentials(decode)
+        user = self.user_object_from_credentials(usr, pwd)
+        return user
