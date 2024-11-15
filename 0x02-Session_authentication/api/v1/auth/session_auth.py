d0b7eaabdc1e487ed76904086a049ce5053d9f2d
abraham <wole1702@gmail.com>
2024-11-15 01:04:27 +0100

    create_sesssino

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 """ Sessino authentication """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """ Session Auth """
-    pass
+    """ Session Auth
+        returns the newly cerated session_id
+    """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+            return None
+        if not (isinstance(user_id, str)):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
