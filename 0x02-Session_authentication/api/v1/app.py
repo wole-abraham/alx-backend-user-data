ef6880046a960fcf49ce111ea5d9941735cb1d37
abraham <wole1702@gmail.com>
2024-11-15 01:48:21 +0100

    fixes

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,7 +38,7 @@ def before_requests():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.session_cookie(request) is None and auth.authorization_header(request) is None:
+    if auth.session_cookie(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
 
