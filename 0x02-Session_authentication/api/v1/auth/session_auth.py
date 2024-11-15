944c351b32daab781511112def74c60bdae6d3af
abraham <wole1702@gmail.com>
2024-11-15 01:14:27 +0100

    permissions

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """ cretes session
+            and returns session id
+        """
         if user_id is None:
             return None
         if not (isinstance(user_id, str)):
