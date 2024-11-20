bf5d816346d4fa67ad9b1c3fcf9e054502666654
abraham <wole1702@gmail.com>
2024-11-20 22:29:14 +0100

    register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ hashed passwords using bcrypt"""
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +13,24 @@ def _hash_password(password: str) -> bytes:
     password = password.encode('utf-8')
     hash = bcrypt.hashpw(password, salt)
     return hash
+
+
+class Auth:
+    """Auth class to interact wiht the authentication database
+    """
+
+    def __init__(self):
+        """__db = DB()
+
+        """
+        self._db = DB()
+
+    def register_user(self, email, password):
+        """ register user -> User <- email, password
+        """
+        user = self._db.find_user_by(email=email)
+        if not user:
+            hashed = _hash_password(password)
+            user = self._db.add_user(email, hashed)
+            return user
+        raise ValueError(f"User <{email}> already exists")
