d937a054c1280624efad95c8bed4ee22052383cf
abraham <wole1702@gmail.com>
2024-11-21 20:42:01 +0100

    user_pprofile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,17 @@ def login_logout():
         return redirect("/")
 
 
+@app.route("/profile")
+def profile():
+    """
+    profile: if exists, 200
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
