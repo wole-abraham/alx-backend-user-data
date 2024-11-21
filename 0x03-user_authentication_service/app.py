9300e80555bb70095b58c48a9819c4492187a963
abraham <wole1702@gmail.com>
2024-11-21 21:15:09 +0100

    reset_password_token_route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,12 +71,12 @@ def get_reset_password_token():
     reset password
 
     """
-    email = request.form.get('email')
-    user = AUTH._db.find_user_by(email=email)
-    if not user:
+    try:
+        email = request.form.get('email')
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
         abort(403)
-    reset_token = AUTH.get_reset_password_token(email)
-    return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
 if __name__ == "__main__":
